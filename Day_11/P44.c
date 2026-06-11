1e5483e32fe18528747fd1ee3fec0b79f6a9b93d
princy <princy4962@gmail.com>
2026-06-11 08:51:15 -0700

    summer_assignment_Day_11

---
+++ Day_11/P44.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+unsigned long long findFactorialIterative(int n);
+
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Error: Factorial of a negative number doesn't exist.\n");
+    } else {
+        unsigned long long result = findFactorialIterative(num);
+        printf("Factorial of %d = %llu\n", num, result);
+    }
+    return 0;
+}
+unsigned long long findFactorialIterative(int n) {
+    unsigned long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
